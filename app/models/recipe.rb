a3eadea7dac3f90ba493fb9c2da34c2d1fd814ea
Aditi Kilambi <kilambi.aditi@gmail.com>
2018-09-03 14:57:58 -0700

    Edited recipes.rb to hopefully fix JSON error

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,9 +3,14 @@ require 'httparty'
 class Recipe
   include HTTParty
 
-  default_params key: ENV["FOOD2FORK_KEY"]
+
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
+
+  default_params key: ENV["FOOD2FORK_KEY"],
+                 fields: "image_url, title, social_rank",
+                 q: "search"
+  
   format :json
 
   def self.for (keyword)
